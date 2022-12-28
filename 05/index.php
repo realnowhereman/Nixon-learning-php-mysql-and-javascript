7c0b9212f17d3cf0938e039ddcbe7b9d4a4b7a37
Michail K <learnprogramming15@gmail.com>
2022-12-28 20:58:44 +0300

    added link to php server autoreload guide

--- 05/index.php
+++ 05/index.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
 <?php
   //Ch-05
   echo date("l") . "<br>";
@@ -5,4 +14,7 @@
   echo strrev(".dlrow olleH") . "<br>";
 echo str_repeat("Hip", 2) . "<br>";
 echo strtoupper("hooray!");
+echo "<hr><a href=\"https://stackoverflow.com/questions/60678203/is-it-possible-to-use-live-server-for-php-with-autoreload-on-save\" target=\"_blank\">php server autoreload</a><hr>"
 ?>
+</body>
+</html>
