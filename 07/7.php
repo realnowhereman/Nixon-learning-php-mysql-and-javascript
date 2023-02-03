281908df66926f136a916f997ca0f02dc37cf0ba
Michail K <learnprogramming15@gmail.com>
2023-02-04 00:08:13 +0300

    started ch-07

---
+++ 07/7.php
@@ -0,0 +1,18 @@
+<?php
+  printf('В вашей корзине находится %d покупки', 3);
+  echo '<br>';
+  printf('В вашей корзине находится %f покупки', 3);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>7</title>
+</head>
+<body>
+  
+</body>
+</html>
