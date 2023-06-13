9ae388298b50b726501ad36a6e0b225dfaad74cf
Michail K <learnprogramming15@gmail.com>
2023-06-13 19:37:36 +0300

    started work on ch-04

---
+++ 4/4.php
@@ -0,0 +1,16 @@
+<?php
+  echo "<h2>Hello ch-04!</h2>";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>4</title>
+</head>
+<body>
+  
+</body>
+</html>
